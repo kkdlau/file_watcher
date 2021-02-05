c7839b065f2a531c3e837b1f2f1790338ac4a5e4
kkdlau <danny1251597530@gmail.com>
2021-02-05 16:56:04 +0800

    add readme.md

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-require('log-timestamp');
+// require('log-timestamp');
 
 const fileToWatch = './F429_Coreboard.elf';
 
 console.log('watching ' + fileToWatch);
 
 fs.watchFile(fileToWatch, (cur, prev) => {
-    console.log(fileToWatch + ' has changed!');
-    console.log('start flashin code!')
+    // console.log(fileToWatch + ' has changed!');
+    // console.log('start flashin code!')
     exec("bash ./flash.sh", (err, stdout, stderr) => {
         console.log(stdout);
         console.error(stderr);
---
+++ readme.md
@@ -0,0 +1,18 @@
+# File Watcher
+
+Tiny application for listening to file changes and execute temrinal commands.
+
+## Installation
+
+You will need to `npm` & `nodejs` for running this application. After installed `npm` & `nodejs`, run:
+
+```bash
+npm install
+```
+
+It will install all packages you need for this application.
+
+To run it, enter:
+```bash
+node index.js
+```
